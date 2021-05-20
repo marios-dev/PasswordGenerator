76192354a5a5c37b3e118ca157a86dda27e39e5b
Mario Syti <msyti@outlook.com>
2021-05-20 23:26:48 +0300

    marios15

--- pw/src/app/app.component.ts
+++ pw/src/app/app.component.ts
@@ -3,15 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  password='';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
 
-
-
-  onButtonClick(){
-    this.password='My Password';
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
   }
-  
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.includeSymbols=!this.includeSymbols;
+  }
+
+  onButtonClick() {
+    console.log(this.includeLetters);
+    console.log(this.includeNumbers);
+    console.log(this.includeSymbols);
+    this.password = 'My Password';
+  }
 }
